59de164957aff2e54ac8a21bd3fa31d82116bc52
Josue-Tech-dev <concentrajosue@gmail.com>
2026-09-23 06:50:35 +0200

    Add exercise ex0016.py

---
+++ ex016.py
@@ -0,0 +1,6 @@
+from math import trunc
+
+num = float(input('Digite um número real: \n'))
+
+print('O valor digitado é {} e a sua porção inteira é {}.'.format(num, int(num)))
+print('O valor digitado é {} e a sua porção inteira é {}.'.format(num, trunc(num)))
