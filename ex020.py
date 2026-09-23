8845a84c8b3035cec4ecfeade40309fd1b96f626
Josue-Tech-dev <concentrajosue@gmail.com>
2026-09-23 07:04:21 +0200

    Add exercise ex020.py

---
+++ ex020.py
@@ -0,0 +1,13 @@
+from random import shuffle
+
+n1 = str(input('Primeiro aluno: \n'))
+n2 = str(input('Segundo aluno: \n'))
+n3 = str(input('Terceiro aluno: \n'))
+n4 = str(input('Quarto aluno: \n'))
+
+lista = [n1, n2, n3, n4]
+
+shuffle(lista)
+
+print('A ordem de apresentação será:')
+print(lista)
