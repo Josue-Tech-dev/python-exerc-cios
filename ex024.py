31047a05a6acd8b81a2b3bf02aee8533cfbd8b7a
Josue-Tech-dev <concentrajosue@gmail.com>
2026-09-25 22:01:22 +0200

    Add exercise ex024.py

---
+++ ex024.py
@@ -0,0 +1,5 @@
+cidade = str(input('Em que cidade você nasceu? \n')).strip()
+print(cidade[:5].upper() =='SANTO')
+
+nome = str(input('Qual é o seu nome:')).strip()
+print(nome[:5].upper() =='JOSUE')
