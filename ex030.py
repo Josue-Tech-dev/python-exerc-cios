4e2edceda76a42040bd1b12e82c3cca6e2495445
Josue-Tech-dev <concentrajosue@gmail.com>
2026-09-26 20:44:49 +0200

    Add exercise ex030.py

---
+++ ex030.py
@@ -0,0 +1,8 @@
+num = int(input('Digite um número: \n'))
+
+resultado = num % 2
+
+if resultado == 0:
+    print('O número {} é PAR.'.format(num))
+else:
+    print('O número {} é ÍMPAR.'.format(num))
